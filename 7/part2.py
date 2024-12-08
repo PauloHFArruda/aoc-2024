66d81fb0e1f8c69c36d352f058be7e342e976ce4
Paulo Henrique Arruda <paulohenrique.arruda656@gmail.com>
2024-12-08 10:20:02 -0300

    feat: solution day 7

---
+++ 7/part2.py
@@ -0,0 +1,29 @@
+import sys
+
+equations = open(sys.argv[1]).read().split("\n")
+
+
+def check(result, operands, acc):
+    if len(operands) == 0:
+        return result == acc
+
+    return (
+        check(result, operands[1:], acc + operands[0])
+        or check(result, operands[1:], acc * operands[0])
+        or check(result, operands[1:], int(str(acc) + str(operands[0])))
+    )
+
+
+res = 0
+for eq in equations:
+    if eq == "":
+        continue
+
+    result, values = eq.split(": ")
+
+    operands = list(map(int, values.split(" ")))
+
+    if check(int(result), operands[1:], operands[0]):
+        res += int(result)
+
+print(res)
